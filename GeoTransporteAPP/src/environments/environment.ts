9bb805dad25bbd5fc4f7e0eb645e2fcfe7dc50f7
MARSANCAL <129116787+MARSANCAL@users.noreply.github.com>
2024-09-29 18:23:38 -0300

    Base de datos Firebase y CRUD de vehículo ('admin-vehicle')

--- GeoTransporteAPP/src/environments/environment.ts
+++ GeoTransporteAPP/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAo0rQPfzmShwbPHg9cCzjCgqv9w0FdQgQ",
+    authDomain: "geo-trans-4e024.firebaseapp.com",
+    projectId: "geo-trans-4e024",
+    storageBucket: "geo-trans-4e024.appspot.com",
+    messagingSenderId: "949019105855",
+    appId: "1:949019105855:web:089f6ad617be67fa35f68a",
+    measurementId: "G-FD93ZHFVWQ"
+  }
 };
 
 /*
