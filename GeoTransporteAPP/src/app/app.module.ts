5d51d0a8a9e2c84efebf7634b71dc901065f0c7d
MARSANCAL <129116787+MARSANCAL@users.noreply.github.com>
2024-11-11 22:03:15 -0300

    Guardar cambios antes de cambiar de master

--- GeoTransporteAPP/src/app/app.module.ts
+++ GeoTransporteAPP/src/app/app.module.ts
@@ -21,7 +21,8 @@ import { environment } from 'src/environments/environment.prod';
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
-    provideFirebaseApp(() => initializeApp({"projectId":"geo-trans-4e024","appId":"1:949019105855:web:089f6ad617be67fa35f68a","storageBucket":"geo-trans-4e024.appspot.com","apiKey":"AIzaSyAo0rQPfzmShwbPHg9cCzjCgqv9w0FdQgQ","authDomain":"geo-trans-4e024.firebaseapp.com","messagingSenderId":"949019105855","measurementId":"G-FD93ZHFVWQ"}))],
+    provideFirebaseApp(() => initializeApp({"projectId":"geo-trans-4e024","appId":"1:949019105855:web:089f6ad617be67fa35f68a","storageBucket":"geo-trans-4e024.appspot.com","apiKey":"AIzaSyAo0rQPfzmShwbPHg9cCzjCgqv9w0FdQgQ","authDomain":"geo-trans-4e024.firebaseapp.com","messagingSenderId":"949019105855","measurementId":"G-FD93ZHFVWQ"}))
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
